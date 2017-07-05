5cb9af36f47de276d7939e6572f9299f16a8ce07
jessicacris <jessica.cris1127@gmail.com>
2017-07-05 16:14:50 -0300

    Create fronze spot

--- lib/allocation/allocation_controller.rb
+++ lib/allocation/allocation_controller.rb
@@ -1,48 +1,32 @@
 require 'sinatra/base'
 require_relative 'allocation'
-require_relative '../base/resources_base'
-require_relative '../base/class_type_error'
+require_relative 'base_allocation'
 
 
 class AllocationBaseApp < Sinatra::Base
-
-	def model
-		AllocationModel
-	end
-
-  before do
-    content_type :json
-  end
+	include AllocationBase
 
   get '/api/allocations/' do
-    [200, AllocationModel.all.to_json]
+  	allocations
   end
 
   get '/api/allocations/user/:id' do |id|
-    [200, AllocationModel.all(user_model_id: id).to_json]
+    get_user_allocations(id)
   end
 
 	get '/api/allocations/resource/:id' do |id|
-    [200, AllocationModel.all(resource_model_id: id).to_json]
+    get_resource_allocations(id)
   end
 
 	get '/api/allocations/:id' do |id|
-    [200, AllocationModel.get(id).to_json ]
+    allocation(id)
   end
 
   post '/api/allocations/' do
-		data = JSON.load(request.body.read)
-		allocation = AllocationModel.new(data)
-		if allocation.valid?
-			allocation.save
-			[201, 'Resource created']
-		else
-			[403, 'Bad format in data send']
-		end
+		allocate(request)
   end
 
   delete '/api/allocations/:id' do |id|
-		AllocationModel.get(id).destroy!
-    [202, "Record Delete"]
+		deallocate(id)
   end
 end
